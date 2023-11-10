9357427aa32210b202fabc3e5f679588def4dc2b
Ethan Lynch <ethanlynch72@gmail.com>
2023-11-10 00:00:54 -0500

    update

--- Classes_Objects.py
+++ Classes_Objects.py
@@ -13,6 +13,8 @@ class Worker:
         return self.employee_number
         
     def set_employee_number(self, employee_number):
+        if not isinstance(employee_number, int):
+            raise Exception("Invalid employee number")
         self.employee_number = employee_number
 
     def get_office_number(self):
@@ -20,14 +22,18 @@ class Worker:
         
     def set_office_number(self, office_number):
         if office_number < 100 or office_number > 500:
-            print("Invalid office number")
-            return False
+            raise Exception("Invalid office number")
         self.office_number = office_number
 
     def get_name(self):
         return self.name
         
+   
     def set_name(self, name):
+        if name == None or name == "":
+            raise Exception("Invalid name")
+        if "_" in name or "-" in name or "." in name:
+            raise Exception("Invalid name")
         self.name = name
 
     def get_birth_date(self):
@@ -35,15 +41,17 @@ class Worker:
         
     def set_birth_date(self, m, d, y):
         if m < 1 or m > 12:
-            if d < 1 or d > 31:
-                print("Invalid day")
-                return False
+            raise Exception("Invalid day")
+        if d < 1 or d > 31:   
+            raise Exception("Invalid day")
         self.birth_date = (m, d, y)
     
     def get_hours_worked(self):
         return self.hours_worked
     
     def add_hours_worked(self, hours_worked):
+        if hours_worked < 0:
+            raise Exception("Invalid hours worked")
         if hours_worked > 9:
             self.overtime_hours += hours_worked - 9
             self.hours_worked += 9
@@ -73,3 +81,6 @@ class Worker:
     
     def get_pay(self):
         return self.hourly_salary * self.hours_worked + self.overtime_hourly_salary * self.overtime_hours
+    
+
+    
